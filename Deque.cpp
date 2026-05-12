a2b01891f0ed45063d13537900ba3e9fae350214
ShreyashWillCode <shreyanshmahato83683@gmail.com>
2026-05-12 23:55:43 +0530

    tried deque

---
+++ Deque.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int main()
+{deque<int>d={1,2,3,4,};
+for (int i = 0; i < d.size(); i++)
+{
+    cout<<d[i]<<" ";
+}
+cout<<endl;
+d.push_front(1);
+
+d.push_back(3);
+for (int i = 0; i < d.size(); i++)
+{
+    cout<<d[i]<<" ";
+}
+cout<<endl;
+    return 0;
+}
