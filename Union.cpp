395b4b095f0a67122145c10499a2d03829a82587
ShreyashWillCode <shreyanshmahato83683@gmail.com>
2026-05-19 23:36:54 +0530

    solving union array problem

---
+++ Union.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        int i=0;j=0;
+        int m=nums1.size();
+        int n=nums2.size();
+        while(i<m && j<n){
+            if(nums1[i])
+            if(unionArray.empty()||unionArray.back()!=nums1[i]){
+                unionArray.push_back(nums1[i]);
+            }
+            if()
+        }
+    }
+int main()
+{vector<int>num1={1,2,3,4,5};
+vector<int>num2={3,4,7};
+
+
+
+    return 0;
+}
