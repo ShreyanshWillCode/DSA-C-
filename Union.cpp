064c523e7bed9b6d365ae996d2d245a2a59f49ad
ShreyashWillCode <shreyanshmahato83683@gmail.com>
2026-05-28 13:03:30 +0530

    Union array

--- Union.cpp
+++ Union.cpp
@@ -8,23 +8,24 @@ vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
         int n=nums2.size();
         vector<int>Union;
         while(i<m && j<n){
-        if(nums1[i]<nums2[j]){
-         if(Union.empty()||Union.back()!=nums1[i]){
+            if(nums1[i]<nums2[j]){
+            if(Union.empty()||Union.back()!=nums1[i])
                 Union.push_back(nums1[i]); 
-                
-            }
-            i++;}
+            i++;
+         }
             else if(nums2[j]<nums1[i]){
-               Union.push_back(nums2[j]); 
+                if(Union.empty()||Union.back()!=nums2[j])
+                    Union.push_back(nums2[j]); 
             j++;
             
             }
             else{
-            if(Union.empty()||Union.back()!=nums1[i]){
+            if(Union.empty()||Union.back()!=nums1[i])
                 Union.push_back(nums1[i]); 
             i++;
             j++;
-            }}
+            }
+        }
             while(i<m){
                 if(Union.empty()||Union.back()!=nums1[i])
                     Union.push_back(nums1[i]);
@@ -36,10 +37,10 @@ vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
                 j++;
 
             }
-
+ return Union;
         }
-        return Union;
-    }
+       
+    
 int main()
 {vector<int>num1={1,2,3,4,5};
 vector<int>num2={1,2,7};
